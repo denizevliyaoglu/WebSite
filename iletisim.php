955bc4dfdd151f815c97462ae8a63adfa97589cb
denizevliyaoglu <deniz.evliyaogl53@gmail.com>
2021-06-17 15:50:52 +0300

    iletisim.php last update

--- iletisim.php
+++ iletisim.php
@@ -4,10 +4,10 @@
               <?php
               if(isset($_POST['submit'])){
                      print_r($_POST);
-                     echo "GÖRÜŞ VE ÖNERİLERİNİZ DİKKATE ALINACAKTIR. TEŞEKKÜR EDERİZ.";
+                     echo "İŞLEM BAŞARISIZ. TEKRAR DENEYİNİZ.";
               }
               else{
-                     echo "İŞLEM BAŞARISIZ. TEKRAR DENEYİNİZ.";
+                     echo "GÖRÜŞ VE ÖNERİLERİNİZ DİKKATE ALINACAKTIR. TEŞEKKÜR EDERİZ.";
               }
               ?>
        </body>
