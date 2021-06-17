fbfa7525d1734786cb26d1ae9504daf9b4575dae
denizevliyaoglu <deniz.evliyaogl53@gmail.com>
2021-06-17 15:48:07 +0300

    anket.php last update

--- anket.php
+++ anket.php
@@ -4,10 +4,10 @@
               <?php
               if(isset($_POST['submit'])){
                      print_r($_POST);
-                     echo "ANKETİ DOLDURDUĞUNUZ İÇİN TEŞEKKÜR EDERİZ.";
+                     echo "İŞLEM BAŞARISIZ. TEKRAR DENEYİNİZ.";
               }
               else{
-                     echo "İŞLEM BAŞARISIZ. TEKRAR DENEYİNİZ.";
+                     echo "ANKETİ DOLDURDUĞUNUZ İÇİN TEŞEKKÜR EDERİZ.";
               }
               ?>
        </body>
